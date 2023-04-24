2f13d9222ac664db9a9851bfd364ab61f6437778
Daana Roc <rocdaana27@gmail.com>
2023-04-24 02:25:36 +0200

    plan genere pour problem basique

--- src/fr/uga/pddl4j/examples/sat/SATEncoding.java
+++ src/fr/uga/pddl4j/examples/sat/SATEncoding.java
@@ -1,61 +1,222 @@
 package fr.uga.pddl4j.examples.sat;
 
-import fr.uga.pddl4j.problem.Fluent;
+
 import fr.uga.pddl4j.problem.Problem;
 import fr.uga.pddl4j.problem.operator.Action;
-import fr.uga.pddl4j.problem.operator.Condition;
-import fr.uga.pddl4j.util.BitVector;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class SATEncoding {
-    Problem p;
-    public SATEncoding(Problem problem){
+	Problem p;
 
-        this.p = problem;
-    }
-    public List<List<String>> Encode(int nbsteps){
-        List<Object> ob = new ArrayList<>();
-        
-        
-        for(int i=0;i<nbsteps;i++) {
-	        for (int j = 0; j < p.getActions().size(); j++) {
-	            Action a = p.getActions().get(j);
-	            ob.add(a);
-	        }
-	        for (int j=0;j < p.getFluents().size();j++) {
-	        	Fluent f = p.getFluents().get(j);
-	        	ob.add(f);
-	        }
-        }
-        
-        //ob contient la liste des action et des fluent pour chaque étape
-        //on construit les clause
-        for(int c=0;c<ob.size();c++) {
-        	int depth = (c / (p.getActions().size()+p.getFluents().size()))-1;//pb
-        	System.out.print("depht "+depth);
-        	if(ob.get(c).getClass()==Action.class) {
-        		
-        	}
-        }
-        
-        
-        return null;
-    }
+	public SATEncoding(Problem problem){
 
+		this.p = problem;
+	}
+	public List<List<Integer>> Encode(int nbsteps){
+
+
+		List<List<Integer>> clauses = new ArrayList<>();
+		int t =(p.getActions().size()+p.getFluents().size());
+		//		Object[] var = new Object[nbsteps*t+1];
+		//		int j=1;
+		//		for(int i=0;i<nbsteps;i++) {
+		//			int flu=0;
+		//			while (j <=i*t+p.getFluents().size()) {
+		//				Fluent a = p.getFluents().get(flu);
+		//				var[j]=a;
+		//				j++;
+		//				flu++;
+		//			}
+		//			int act=0;
+		//			while (j <= (i+1)*t) {
+		//				Action a = p.getActions().get(act);
+		//				var[j]=a;
+		//				j++;
+		//				act++;
+		//			}
+		//		}
+
+
+
+		for (int k=0; k<p.getFluents().size();k++) {
+			List<Integer> clInit= new ArrayList<>();
+			if(p.getInitialState().getPositiveFluents().get(k)) {
+				clInit.add(k+1);
+				clauses.add(clInit);
+			}else {
+				clInit.add(0-k-1);
+				clauses.add(clInit);
+			}
+
+		}
+
+
+		//parcourir les actions
+		int fSize = p.getFluents().size();
+		for(int step=0; step<nbsteps; step++) {
+			for(int d =fSize+t*step; d<t*(step+1); d++) {
+
+				//fix to debug
+				Action a = p.getActions().get(d-(t*step)-fSize);
+				if(a.getUnconditionalEffect().getNegativeFluents().get(0)!= a.getUnconditionalEffect().getPositiveFluents().get(0)) {
+
+
+					//parcourir les preconditions de chaque action
+					String pStringValues = a.getPrecondition().getPositiveFluents().toString();
+					String[] preconditions = pStringValues.substring(1, pStringValues.length() - 1).split(", ");
+					for(int e=0; e< preconditions.length; e++) {
+						List<Integer> cl= new ArrayList<>();
+						cl.add(-d-1);
+						cl.add((Integer.parseInt(preconditions[e])+1)+t*step);
+						clauses.add(cl);
+
+					}
+
+					//parcourir les effets positifs
+					String pEffetPositifs = a.getUnconditionalEffect().getPositiveFluents().toString();
+					String[] effetpositifs = pEffetPositifs.substring(1, pEffetPositifs.length() - 1).split(", ");
+					String pEffetNegatifs = a.getUnconditionalEffect().getNegativeFluents().toString();
+					String[] effetNegatifs = pEffetNegatifs.substring(1, pEffetNegatifs.length() - 1).split(", ");
+
+					for(int e=0; e< effetpositifs.length; e++) {
+						if(!Arrays.asList(effetNegatifs).contains(effetpositifs[e])) {
+							List<Integer> cl= new ArrayList<>();
+							cl.add(-d-1);
+							cl.add((Integer.parseInt(effetpositifs[e])+1)+t*(step+1));
+							clauses.add(cl);
+						}
+
+					}
+					//parcourir les effets negatifs
+
+					for(int e=0; e< effetNegatifs.length; e++) {
+						if(!Arrays.asList(effetpositifs).contains(effetNegatifs[e])) {
+							List<Integer> cl= new ArrayList<>();
+							cl.add(-d-1);
+							int result = (Integer.parseInt(effetNegatifs[e])+1)+t*(step+1);
+							cl.add(0-result);
+							clauses.add(cl);
+						}
+
+					}
+
+
+				}
+			}
+
+		}
+		//fluentAction contient une liste de Fluent contenant chaque action qui peut les modifier 
+		HashMap<Integer, List<Integer>> fluentAction = new HashMap<>();
+		System.out.print(p.getFluents().size());
+		for(int clause =p.getFluents().size(); clause < clauses.size();clause++) {
+			List<Integer> cl = clauses.get(clause);
+			for(int fluent = 1; fluent<cl.size(); fluent++) {
+				List<Integer> tmp = new ArrayList<>();
+				if(fluentAction.containsKey(cl.get(fluent))) {
+					fluentAction.get(cl.get(fluent)).add(0-cl.get(0));
+				}
+				else {
+					
+					tmp.add(0-cl.get(0));
+					fluentAction.put(cl.get(fluent), tmp);
+				}
+			}
+
+
+		}
+
+		//parcourir les fluents positifs 
+		for(Integer key: fluentAction.keySet()) {
+			int next = key+t; 
+			List<Integer> numValue =  fluentAction.get(next);
+			List<Integer> numValue2 =  fluentAction.get(-next);
+			List<Integer> cl = new ArrayList<>();
+			List<Integer> cl2 = new ArrayList<>();
+
+
+			cl.add(key);
+			cl.add(0-(key+t));
+
+			cl2.add(0-(key));
+			cl2.add(key+t);
+
+			if(key>((t-1)*nbsteps) ){
+
+			}else {
+				if(numValue != null) {
+					for(Integer num : numValue) {
+						cl.add(num);
+					}
+					if(Math.abs(cl.get(0))< Math.abs(cl.get(1))) {
+						clauses.add(cl);
+					}
+					
+				}
+				if(numValue2 != null) {
+					for(Integer num : numValue2) {
+						cl2.add(num);
+
+					}
+					if(Math.abs(cl2.get(0))< Math.abs(cl2.get(1))) {
+						clauses.add(cl2);
+					};
+				} 
+
+			}
+		}
+
+		//		//Goal
+				for (int k=0; k<p.getFluents().size();k++) {
+					List<Integer> cl= new ArrayList<>();
+					if(p.getGoal().getPositiveFluents().get(k)) {
+						cl.add(k+1+(t*nbsteps));
+						clauses.add(cl);
+					}else {
+						cl.add(0-k-1-(t*nbsteps));
+						clauses.add(cl);
+					}
+					
+				}
+
+		// Disjunction
+				for(int step=0; step<nbsteps; step++) {
+					List<Integer> cl= new ArrayList<>();
+					for(int d =fSize+t*step; d<t*(step+1); d++) {
+						cl.add(-d-1);
+					}
+					clauses.add(cl);
+				}
+
+
+
+
+		//		if(p.getGoal().getPositiveFluents().cardinality()>0) {
+		//			
+		//			String str = p.getGoal().getPositiveFluents().toString();
+		//			String[] goal = str.substring(1, str.length() - 1).split(", ");
+		//			
+		//			for(String g : goal) {
+		//				List<Integer> g1 =new ArrayList<>();
+		//				g1.add(Integer.parseInt(g)+t*nbsteps+1);
+		//				clauses.add(g1);
+		//			}
+		//		}
+		//		if(p.getGoal().getNegativeFluents().cardinality()>0) {
+		//			String str1 = p.getGoal().getNegativeFluents().toString();
+		//
+		//			String[] goal1 = str1.substring(1, str1.length() - 1).split(", ");
+		//			for(String g : goal1) {
+		//				int val = Integer.parseInt(g)+t*nbsteps-1;
+		//				List<Integer> g1 =new ArrayList<>();
+		//				g1.add(0-val);
+		//				clauses.add(g1);
+		//			}
+		//		}
+
+		return clauses;
+	}
 }
-//    BitVector pos = a.getUnconditionalEffect().getPositiveFluents();
-//    BitVector neg = a.getUnconditionalEffect().getNegativeFluents();
-//    BitVector precondneg =  a.getPrecondition().getNegativeFluents();
-//    BitVector precondpos = a.getPrecondition().getPositiveFluents();
-//int[] param = a.getParameters();
-//List<String> predicateSymbols = p.getPredicateSymbols();
-//// System.out.println(predicateSymbols);
-//Condition goal = p.getGoal();
-//List<String> types =  p.getTypes();
-//for (int i = 0; i < p.getTypes().size(); i++) {
-////    System.out.println(p.getTypes().get(i).toString());
-//}
-//
--- src/fr/uga/pddl4j/examples/sat/SATMain.java
+++ src/fr/uga/pddl4j/examples/sat/SATMain.java
@@ -2,11 +2,23 @@ package fr.uga.pddl4j.examples.sat;
 
 import fr.uga.pddl4j.parser.DefaultParsedProblem;
 import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.plan.SequentialPlan;
 import fr.uga.pddl4j.planners.AbstractPlanner;
 import fr.uga.pddl4j.problem.DefaultProblem;
 import fr.uga.pddl4j.problem.Problem;
+import fr.uga.pddl4j.problem.operator.Action;
+
+import java.util.List;
+import java.util.ArrayList;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.sat4j.core.VecInt;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.ISolver;
+import org.sat4j.specs.TimeoutException;
+
 import picocli.CommandLine;
 
 
@@ -29,7 +41,8 @@ import picocli.CommandLine;
         optionListHeading = "%nOptions:%n")
 public class SATMain extends AbstractPlanner {
 
-    /**
+    private static final long serialVersionUID = 1L;
+	/**
      * The class logger.
      */
     private static final Logger LOGGER = LogManager.getLogger(SATMain.class.getName());
@@ -44,9 +57,6 @@ public class SATMain extends AbstractPlanner {
     public Problem instantiate(DefaultParsedProblem problem) {
         final Problem pb = new DefaultProblem(problem);
         pb.instantiate();
-//        System.out.println( "size action"+pb.getActions().get(0).getName());
-//        System.out.println( "size action"+pb.getActions().get(1).getName());
-//        this.solve(pb);
 
         return pb;
     }
@@ -61,8 +71,83 @@ public class SATMain extends AbstractPlanner {
     public Plan solve(final Problem problem) {
 //        System.out.println("mi22 test"+problem.getActions());
         SATEncoding satEncoding = new SATEncoding(problem);
-        satEncoding.Encode(2);
-        return null;
+        List<List<Integer>> clauses = satEncoding.Encode(1);
+//        List<List<Integer>> clauses = new ArrayList<>();
+//        List<Integer> clause1 = new ArrayList<>();
+//        clause1.add(-1);
+//        clause1.add(-2);
+//        List<Integer> clause2 = new ArrayList<>();
+//        clause2.add(1);
+//        List<Integer> clause3 = new ArrayList<>();
+//        clause3.add(-2);
+//        clauses.add(clause1);
+//        clauses.add(clause2);
+//        clauses.add(clause3);
+        ISolver solver = SolverFactory.newDefault();
+        solver.setTimeout(300); // 1 hour timeout
+        final int MAXVAR = 1000000;
+        final int NBCLAUSES = 500000;
+
+        
+        // prepare the solver to accept MAXVAR variables. MANDATORY for MAXSAT solving
+        solver.newVar(MAXVAR);
+        solver.setExpectedNumberOfClauses(NBCLAUSES);
+        // Feed the solver using Dimacs format, using arrays of int
+        // (best option to avoid dependencies on SAT4J IVecInt)
+        
+        
+        for (List<Integer> clause : clauses) {
+            int[] intClause = clause.stream().mapToInt(i -> i).toArray();
+            try {
+            	VecInt v = new VecInt(intClause);
+            	System.out.println(" heeooo   "+v);
+				solver.addClause(v);
+				
+			} catch (ContradictionException e) {
+				// TODO Auto-generated catch block
+			//	e.printStackTrace();
+			}
+        }
+
+        // we are done. Working now on the IProblem interface
+//        IProblem problem = solver;
+//        if (problem.isSatisfiable()) {
+//           ....
+//        } else {
+//         ...
+//        }
+     // Find a satisfying assignment (a plan)
+        
+        List<Integer> plan = new ArrayList<Integer>();
+        try {
+            if (solver.isSatisfiable()) {
+                int[] model = solver.model();
+                for (int literal : model) {
+                    if (literal > 0) {
+                        plan.add(literal);
+                    }
+                }
+            }
+        } catch (TimeoutException e) {
+            // Handle timeout exception
+            e.printStackTrace();
+        }
+       
+        int t =(problem.getActions().size()+problem.getFluents().size());
+        Plan newPlan = new SequentialPlan();
+        
+        for(int literal : plan) {
+        	int actionId = (literal%t) - problem.getFluents().size();
+        	if( actionId>= problem.getFluents().size()) {
+        		Action a = problem.getActions().get(actionId-1);
+        		
+        		 newPlan.add(Math.round(literal/t), a);
+        	}
+        	
+        }
+        
+        
+        return newPlan;
     }
 
     @Override
@@ -77,9 +162,12 @@ public class SATMain extends AbstractPlanner {
      */
     public static void main(String[] args) {
         try {
+        	
             final SATMain planner = new SATMain();
+    
             CommandLine cmd = new CommandLine(planner);
             cmd.execute(args);
+            
         } catch (IllegalArgumentException e) {
             LOGGER.fatal(e.getMessage());
         }
